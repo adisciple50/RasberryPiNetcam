f0df4b8e54afd9886612bd4ec85fa3443891cd9b
deddokatana <deddokatana@gmail.com>
2016-02-13 00:12:38 +0000

    Added Image Captures

--- Main.py
+++ Main.py
@@ -1,8 +1,8 @@
-import RPi.GPIO as GPIO
-import time
-import subprocess
-
-
+import RPi.GPIO as GPIO # access the gpio button
+import time # deals with time delays
+import subprocess # issues console commands
+from SimpleCV import Camera # captures images
+import os # file operations
 
 # wake from halt
 # http://raspberrypi.stackexchange.com/questions/13203/creating-halt-wake-button
@@ -10,9 +10,13 @@ import subprocess
 # "I have rigged up a wake-from-halt button using pins 5 and 6."
 # "These two pins, when connected, will reset power and power on from halt(shutdown)".
 
+#initialise camera
+cam = Camera()
 
 
-streaming_time = 30
+## settings
+output_folder = "~/DCIM"
+streaming_time = 600
 # in seconds
 
 GPIO.setmode(GPIO.BOARD)
@@ -32,11 +36,39 @@ def button_is_pressed():
     else:
         return False
 
+# capture image
+# use simplecv
+
+def capture_image():
+    img = cam.getImage()
+    return img
+
+# solution for filename auto increment
+# http://gis.stackexchange.com/a/27414
+def getNextFileName(output_folder):
+    highest_num = 0
+    for f in os.listdir(output_folder):
+        if os.path.isfile(os.path.join(output_folder, f)):
+            file_name = os.path.splitext(f)[0]
+            try:
+                file_num = int(file_name)
+                if file_num > highest_num:
+                    highest_num = file_num
+            except ValueError:
+                'The file name "%s" is not an integer. Skipping' % file_name
+
+    output_file_path = os.path.join(output_folder, str(highest_num + 1))
+    return output_file_path
+
+def save_image(SimpleCVImage=capture_image()):
+    filename = getNextFileName(output_folder)+".png"
+    SimpleCVImage.save(filename)
+
 # method found here:
 # https://sandilands.info/sgordon/live-webca-streaming-using-vlc-command-line
 
 def start_streaming():
-    subprocess.call("cvlc -vvv v4l2:///dev/video0 --sout '#transcode{vcodec=mp2v,vb=800,acodec=none}:rtp{sdp=rtsp://:8554/}'",shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    subprocess.call("vlc -vvv v4l2:///dev/video0 --sout '#transcode{vcodec=mp2v,vb=800,acodec=none}:rtp{sdp=rtsp://:8554/}'",shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 def stream_for_length_of(duration):
     time.sleep(duration)
@@ -53,7 +85,4 @@ def sleep_mode():
 # main function - so run this script on bootup!
 
 if button_is_pressed():
-    start_streaming()
-    stream_for_length_of(streaming_time)
-    stop_streaming()
-    sleep_mode()
+    save_image()
